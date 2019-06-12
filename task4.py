cafa86bffd0ac27bafdcd401b169ed7dcc869897
chyngyzs1 <0777755169ch@gmail.com>
2019-06-12 11:40:35 +0600

    task 4 is done

---
+++ task4.py
@@ -0,0 +1,15 @@
+
+#    WRITE YOUR CODE HERE...
+def last_letter(x): 
+    return(x[-1])
+#DO NOT remove lines below here,
+#this is designed to test your code.
+def test_last_letter():
+  assert last_letter('hello!') == '!'
+  assert last_letter('banana') == 'a'
+  assert last_letter('8') == '8'
+  assert last_letter('funnyguys') == 's'
+  assert last_letter('101') == '1'
+  print("YOUR CODE IS CORRECT!")
+#test your code by un-commenting the line(s) below
+test_last_letter()
